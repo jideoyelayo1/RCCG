5f30643ecbb44d9c36703e5f89c918de9306856b
Jide <JideOyelayo1@Gmail.com>
2023-09-24 10:56:06 +0100

    Update test.py
    Changed the regex to work for verses that start with lowercase letters

--- test.py
+++ test.py
@@ -1,16 +1,15 @@
 import re
 
 def extract_first_verse(text):
-    match = re.search(r'\d+([A-Z].*?)(?=\d+[A-Z]|$)', text)
+    match = re.search(r'\d+([A-Za-z].*?)(?=\d+[A-Za-z]|$)', text)
     if match:
         verse_text = match.group(1).strip()
         return verse_text
     else:
         return None
 
-text = "25And Joseph took an oath from the sons of Israel, saying, God will surely visit you, and you will carry up my bones from here.26So Joseph died, being 110 years old; and they embalmed him, and he was put in a coffin in Egypt."
-text2 = "26So Joseph died, being 110 years old; and they embalmed him, and he was put in a coffin in Egypt."
-result = extract_first_verse(text)
-result2 = extract_first_verse(text2)
-print(result)
-print(result2)
+texts = ["25and Joseph took an oath from the sons of Israel, saying, God will surely visit you, and you will carry up my bones from here.26so Joseph died, being 110 years old; and they embalmed him, and he was put in a coffin in Egypt.", "26So Joseph died, being 110 years old; and they embalmed him, and he was put in a coffin in Egypt.", "26so Joseph died, being 110 years old; and they embalmed him, and he was put in a coffin in Egypt.","25And Joseph took an oath from the sons of Israel, saying, God will surely visit you, and you will carry up my bones from here.26So Joseph died, being 110 years old; and they embalmed him, and he was put in a coffin in Egypt."]
+results = [extract_first_verse(x) for x in texts]
+for result in results:
+	print(1,result)
+
